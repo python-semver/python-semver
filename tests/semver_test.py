2278f7ee0afbc5d235c72d14fda5a1d1baa7f548
Alexander Shorin <kxepal@gmail.com>
2012-02-08 17:15:32 +0400

    Fix Python 3.x deprecation warning about `assertEquals`.

--- tests/semver_test.py
+++ tests/semver_test.py
@@ -8,7 +8,7 @@ from semver import parse
 
 class TestSemver(TestCase):
     def test_should_parse_version(self):
-        self.assertEquals(
+        self.assertEqual(
             parse("1.2.3-alpha.1.2+build.11.e0f985a"),
             {'major': 1,
              'minor': 2,
@@ -17,22 +17,22 @@ class TestSemver(TestCase):
              'build': 'build.11.e0f985a'})
 
     def test_should_get_less(self):
-        self.assertEquals(
+        self.assertEqual(
             compare("1.0.0", "2.0.0"),
             -1)
 
     def test_should_get_greater(self):
-        self.assertEquals(
+        self.assertEqual(
             compare("2.0.0", "1.0.0"),
             1)
 
     def test_should_match_simple(self):
-        self.assertEquals(
+        self.assertEqual(
             match("2.3.7", ">=2.3.6"),
             True)
 
     def test_should_no_match_simple(self):
-        self.assertEquals(
+        self.assertEqual(
             match("2.3.7", ">=2.3.8"),
             False)
 
