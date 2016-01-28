b20db2bd7db32ac8552131035215eb973aaf95b0
Kostiantyn Rybnikov <k-bx@k-bx.com>
2016-01-28 15:23:52 +0200

    2.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name='semver',
-    version='2.2.1',
+    version='2.3.0',
     description='Python package to work with Semantic Versioning (http://semver.org/)',
     long_description=LONG_DESCRIPTION,
     author='Konstantine Rybnikov',
