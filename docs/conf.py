bcd451c9a8927d91b3f37b435a13b2f745dc2ed3
Thomas Schraitle <tom_schr@web.de>
2019-09-30 21:12:30 +0200

    Silence flake8 with "noqa: E402"

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-from semver import __version__
+from semver import __version__  # noqa: E402
 
 # -- General configuration ------------------------------------------------
 
