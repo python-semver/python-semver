2f5a68db22781f8fbece4c4fd43275fcb659715e
Thomas Schraitle <toms@suse.de>
2019-09-30 08:38:56 +0200

    Force major, minor, and patch as int
    To allow VersionInfo(10) > VersionInfo('2') == True

--- semver.py
+++ semver.py
@@ -99,9 +99,9 @@ class VersionInfo(object):
     __slots__ = ('_major', '_minor', '_patch', '_prerelease', '_build')
 
     def __init__(self, major, minor=0, patch=0, prerelease=None, build=None):
-        self._major = major
-        self._minor = minor
-        self._patch = patch
+        self._major = int(major)
+        self._minor = int(minor)
+        self._patch = int(patch)
         self._prerelease = None if prerelease is None else str(prerelease)
         self._build = None if build is None else str(build)
 
