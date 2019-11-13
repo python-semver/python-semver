3cb5be56c8107df7b5b4bc0e3edfaadefabd3f07
Thomas Schraitle <toms@suse.de>
2019-11-13 10:41:25 +0100

    Correct logo width, add white background
    * Add custom CSS file to change size of the logo
    * Add white background

--- docs/conf.py
+++ docs/conf.py
@@ -109,6 +109,8 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = ['css/default.css']
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
