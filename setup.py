61f835f74103419e3ac34bef38150d77a4fb79f8
Tom Schraitle <tomschr@users.noreply.github.com>
2020-10-25 15:04:46 +0100

    Support Python 3.8/3.9 for semver2

--- setup.py
+++ setup.py
@@ -99,6 +99,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
