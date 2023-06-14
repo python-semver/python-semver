17bc2571c4f3c7faf85d5b024d5f9117d555d49b
Dhaval Soneji <dhaval@soneji.xyz>
2023-06-14 11:35:35 +0100

    Fix #410 Export all names in __all__ variable
    * Export all semver names in `__all__` variable.
    * Fix linting issues

--- src/semver/__init__.py
+++ src/semver/__init__.py
@@ -1,5 +1,5 @@
 """
-semver package major release 3.
+Semver package major release 3.
 
 A Python module for semantic versioning. Simplifies comparing versions.
 """
@@ -37,3 +37,36 @@ from .__about__ import (
     __maintainer_email__,
     SEMVER_SPEC_VERSION,
 )
+
+__all__ = [
+    "bump_build",
+    "bump_major",
+    "bump_minor",
+    "bump_patch",
+    "compare",
+    "bump_prerelease",
+    "finalize_version",
+    "format_version",
+    "match",
+    "max_ver",
+    "min_ver",
+    "parse",
+    "parse_version_info",
+    "replace",
+    "cmd_bump",
+    "cmd_compare",
+    "cmd_nextver",
+    "cmd_check",
+    "createparser",
+    "process",
+    "main",
+    "Version",
+    "VersionInfo",
+    "__version__",
+    "__author__",
+    "__maintainer__",
+    "__author_email__",
+    "__description__",
+    "__maintainer_email__",
+    "SEMVER_SPEC_VERSION",
+]
