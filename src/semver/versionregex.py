99667a865efbc8d2b12eb13443be6b5e6934390a
Tom Schraitle <tomschr@users.noreply.github.com>
2023-07-02 19:04:38 +0200

    Change visibility to public of version parts
    * Rename _MAJOR, _MINOR, _PATCH, _PRERELEASE, and _BUILD and
      remove the "_" prefix
    * Change _REGEX, _REGEX_TEMPLATE, and _REGEX_OPTIONAL_MINOR_AND_PATCH
      and remove the "_" prefix

--- src/semver/versionregex.py
+++ src/semver/versionregex.py
@@ -18,46 +18,47 @@ class VersionRegex:
     _LAST_NUMBER: ClassVar[Pattern[str]]  = re.compile(r"(?:[^\d]*(\d+)[^\d]*)+")
 
     #: The regex of the major part of a version:
-    _MAJOR: ClassVar[str] = rf"(?P<major>{_RE_NUMBER})"
+    MAJOR: ClassVar[str] = rf"(?P<major>{_RE_NUMBER})"
     #: The regex of the minor part of a version:
-    _MINOR: ClassVar[str] = rf"(?P<minor>{_RE_NUMBER})"
+    MINOR: ClassVar[str] = rf"(?P<minor>{_RE_NUMBER})"
     #: The regex of the patch part of a version:
-    _PATCH: ClassVar[str] = rf"(?P<patch>{_RE_NUMBER})"
+    PATCH: ClassVar[str] = rf"(?P<patch>{_RE_NUMBER})"
     #: The regex of the prerelease part of a version:
-    _PRERELEASE: ClassVar[str] = rf"""(?P<prerelease>
+    PRERELEASE: ClassVar[str] = rf"""(?P<prerelease>
                 (?:{_RE_NUMBER}|\d*[a-zA-Z-][0-9a-zA-Z-]*)
                 (?:\.(?:{_RE_NUMBER}|\d*[a-zA-Z-][0-9a-zA-Z-]*))*
                 )
     """
+
     #: The regex of the build part of a version:
-    _BUILD: ClassVar[str] = r"""(?P<build>
+    BUILD: ClassVar[str] = r"""(?P<build>
                 [0-9a-zA-Z-]+
                 (?:\.[0-9a-zA-Z-]+)*
             )"""
 
     #: Regex template for a semver version
-    _REGEX_TEMPLATE: ClassVar[str] = rf"""
+    REGEX_TEMPLATE: ClassVar[str] = rf"""
         ^
-        {_MAJOR}
+        {MAJOR}
         (?:
-            \.{_MINOR}
+            \.{MINOR}
             (?:
-                \.{_PATCH}
+                \.{PATCH}
             ){{opt_patch}}
         ){{opt_minor}}
-        (?:-{_PRERELEASE})?
-        (?:\+{_BUILD})?
+        (?:-{PRERELEASE})?
+        (?:\+{BUILD})?
         $
     """
 
     #: Regex for a semver version
-    _REGEX: ClassVar[Pattern[str]] = re.compile(
-        _REGEX_TEMPLATE.format(opt_patch="", opt_minor=""),
+    REGEX: ClassVar[Pattern[str]] = re.compile(
+        REGEX_TEMPLATE.format(opt_patch="", opt_minor=""),
         re.VERBOSE,
     )
 
     #: Regex for a semver version that might be shorter
-    _REGEX_OPTIONAL_MINOR_AND_PATCH: ClassVar[Pattern[str]] = re.compile(
-        _REGEX_TEMPLATE.format(opt_patch="?", opt_minor="?"),
+    REGEX_OPTIONAL_MINOR_AND_PATCH: ClassVar[Pattern[str]] = re.compile(
+        REGEX_TEMPLATE.format(opt_patch="?", opt_minor="?"),
         re.VERBOSE,
     )
