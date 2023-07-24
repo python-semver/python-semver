dc60f88ce9f09a4799939465dbdf5eef799d24de
Tom Schraitle <tomschr@users.noreply.github.com>
2023-07-24 10:06:52 +0200

    Reformatted with black

--- src/semver/versionregex.py
+++ src/semver/versionregex.py
@@ -15,10 +15,16 @@ class VersionRegex:
     _RE_NUMBER: ClassVar[str] = r"0|[1-9]\d*"
 
     #:
-    _LAST_NUMBER: ClassVar[Pattern[str]]  = re.compile(r"(?:[^\d]*(\d+)[^\d]*)+")
+    _LAST_NUMBER: ClassVar[Pattern[str]] = re.compile(r"(?:[^\d]*(\d+)[^\d]*)+")
 
     #: The names of the different parts of a version
-    NAMES: ClassVar[Tuple[str, ...]] = ("major", "minor", "patch", "prerelease", "build")
+    NAMES: ClassVar[Tuple[str, ...]] = (
+        "major",
+        "minor",
+        "patch",
+        "prerelease",
+        "build",
+    )
 
     #: The regex of the major part of a version:
     MAJOR: ClassVar[str] = rf"(?P<major>{_RE_NUMBER})"
@@ -27,20 +33,26 @@ class VersionRegex:
     #: The regex of the patch part of a version:
     PATCH: ClassVar[str] = rf"(?P<patch>{_RE_NUMBER})"
     #: The regex of the prerelease part of a version:
-    PRERELEASE: ClassVar[str] = rf"""(?P<prerelease>
+    PRERELEASE: ClassVar[
+        str
+    ] = rf"""(?P<prerelease>
                 (?:{_RE_NUMBER}|\d*[a-zA-Z-][0-9a-zA-Z-]*)
                 (?:\.(?:{_RE_NUMBER}|\d*[a-zA-Z-][0-9a-zA-Z-]*))*
                 )
     """
 
     #: The regex of the build part of a version:
-    BUILD: ClassVar[str] = r"""(?P<build>
+    BUILD: ClassVar[
+        str
+    ] = r"""(?P<build>
                 [0-9a-zA-Z-]+
                 (?:\.[0-9a-zA-Z-]+)*
             )"""
 
     #: Regex template for a semver version
-    REGEX_TEMPLATE: ClassVar[str] = rf"""
+    REGEX_TEMPLATE: ClassVar[
+        str
+    ] = rf"""
         ^
         {MAJOR}
         (?:
